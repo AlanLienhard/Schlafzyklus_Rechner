cdca24e8334216a0085cc2ade62e2d0f7d2f4a9e
A.Lienhard.inf23 <A.Lienhard.inf23@stud.bbbaden.ch>
2025-06-27 11:19:01 +0200

    Erste Version des Rechners, Funktionen erst teilweise fertig, Klassen für OOP erstellt.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schlafzyklus_Rechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime TimeWakeup;
+            DateTime bedTime = DateTime.MinValue;
+            // Das ist die durchschnittliche Dauer eines Schlafzyklus
+            TimeSpan sleepCycle = TimeSpan.FromMinutes(90);
+
+            Console.WriteLine("Willkommen zum Schlafzyklus Rechner.");
+            Console.WriteLine("Gib ein, wann du aufwachen möchtest [hh:mm]:");
+            TimeWakeup = DateTime.Parse(Console.ReadLine());
+            for (int i = 1; i <= 6; i++) 
+            {
+                bedTime = TimeWakeup - TimeSpan.FromMinutes(90 * i);
+                Console.WriteLine($"Für {i} Zyklen: {bedTime:HH:mm}");
+
+            }
+
+            Console.WriteLine("Du solltest um diese Zeit schlafen gehen: " + bedTime.ToString("HH:mm"));
+
+
+        }
+    }
+}
